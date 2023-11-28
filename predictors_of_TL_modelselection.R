ea8d8f52b8f442d168058696ad716872e5891d86
afan104 <101306676+afan104@users.noreply.github.com>
2023-11-27 19:12:23 -0800

    added wrangled data in data_clean folder as predictors_of_tl_dataset.csv

--- predictors_of_TL_modelselection.R
+++ predictors_of_TL_modelselection.R
@@ -8,4 +8,19 @@ B. Significant predictors
   8) independent residuals: 
       - checked time predictors (date or day_of_the_year or obs no.)
       - checked if need to add back any predictors
+
+C. Visualization
 "
+#############################################################################################################
+# Libraries
+library(corrplot)
+
+# Functions
+#############################################################################################################
+# Accessing data
+tl_df <- read.csv("./data_clean/predictors_of_tl_dataset.csv")
+head(tl_df)
+#############################################################################################################
+# checking for multicollinearity
+X <- model.matrix(ripa_formula, data = ripa_2021)[, -1]
+corrplot(cor(X), type = "lower", tl.cex = 0.65)
